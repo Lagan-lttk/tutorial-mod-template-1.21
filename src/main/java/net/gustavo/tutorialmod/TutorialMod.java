ffbebe21461a23cfd22882782dc57c4b950139a5
Lagan-lttk <almeida.gu018@gmail.com>
2025-08-20 19:09:08 -0300

    Verdin

--- src/main/java/net/gustavo/tutorialmod/TutorialMod.java
+++ src/main/java/net/gustavo/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.gustavo.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.gustavo.tutorialmod.item.ModItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +14,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItem.registerModItem();
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/gustavo/tutorialmod/item/ModItem.java
@@ -0,0 +1,28 @@
+package net.gustavo.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.gustavo.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItem {
+
+    public static final Item BANZA_VERDIN = registerItem("banza_verdin", new Item(new Item.Settings()));
+    public static final Item PRENSADO = registerItem("prensado", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name),item);
+    }
+
+    public static void registerModItem(){
+        TutorialMod.LOGGER.info("Registering mod items for"+ TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+           fabricItemGroupEntries.add(BANZA_VERDIN);
+           fabricItemGroupEntries.add(PRENSADO);
+        });
+    }
+}
