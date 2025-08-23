cc510e7dd4bd6c993f502906f21a915553a372b9
Lagan-lttk <almeida.gu018@gmail.com>
2025-08-23 17:53:10 -0300

    Blocos

--- src/main/java/net/gustavo/tutorialmod/TutorialMod.java
+++ src/main/java/net/gustavo/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.gustavo.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.gustavo.tutorialmod.block.ModBlocks;
 import net.gustavo.tutorialmod.item.ModItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
         ModItem.registerModItem();
-
+        ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/gustavo/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,46 @@
+package net.gustavo.tutorialmod.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.gustavo.tutorialmod.TutorialMod;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    public static final Block BLOCO_DE_MACONHA = registerBlock("bloco_de_maconha",
+            new Block(AbstractBlock.Settings.create().
+                    strength(4f).
+                    breakInstantly().
+                    sounds(BlockSoundGroup.CHERRY_LEAVES)));
+
+    public static final Block BLOCO_DE_GUARANA = registerBlock("bloco_de_guarana",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(1f)
+                    .breakInstantly()
+                    .sounds(BlockSoundGroup.GRASS)));
+
+    public static Block registerBlock(String name, Block block){
+        registerBlockItem(name,block);
+        return Registry.register(Registries.BLOCK, Identifier.of(TutorialMod.MOD_ID,name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block){
+        Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name),
+            new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks(){
+        TutorialMod.LOGGER.info("Registrando blocos para "+ TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.BLOCO_DE_MACONHA);
+        });
+    }
+}
