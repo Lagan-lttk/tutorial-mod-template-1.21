c3c1c7a7ebbd3718bd27766e4826f8d6024f5d1b
Lagan-lttk <almeida.gu018@gmail.com>
2025-08-28 16:39:54 -0300

    Custom ToolTips

--- src/main/java/net/gustavo/tutorialmod/item/ModItem.java
+++ src/main/java/net/gustavo/tutorialmod/item/ModItem.java
@@ -6,15 +6,26 @@ import net.gustavo.tutorialmod.item.custom.ChiselItem;
 import net.gustavo.tutorialmod.item.custom.PoteDeMaizena;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import java.util.List;
 
 public class ModItem {
 
     public static final Item BANZA_VERDIN = registerItem("banza_verdin", new Item(new Item.Settings()));
     public static final Item PRENSADO = registerItem("prensado", new Item(new Item.Settings()));
-    public static final Item MACONHA = registerItem("maconha", new Item(new Item.Settings()));
+    public static final Item MACONHA = registerItem("maconha", new Item(new Item.Settings()){
+        @Override
+        public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+            tooltip.add(Text.translatable("tooltip.tutorialmod.maconha.tooltip"));
+            super.appendTooltip(stack, context, tooltip, type);
+        }
+    });
     public static final Item SEDA = registerItem("seda",new Item(new Item.Settings()));
 
     public static final Item POLENTA = registerItem("polenta", new Item(new Item.Settings().food(ModFoodComponents.POLENTA)));
@@ -28,7 +39,6 @@ public class ModItem {
 
     public static void registerModItem(){
         TutorialMod.LOGGER.info("Registering mod items for"+ TutorialMod.MOD_ID);
-
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
            fabricItemGroupEntries.add(BANZA_VERDIN);
            fabricItemGroupEntries.add(PRENSADO);
